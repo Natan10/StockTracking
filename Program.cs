120d70dbc1dbaffde83c630c8db15f28b0dd7d48
Natan <natan-moreira1@hotmail.com>
2023-10-24 22:11:10 -0300

    feat: config data context and connection string

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using StockTracking.Data;
+
 namespace StockTracking
 {
     public class Program
@@ -13,6 +16,10 @@ namespace StockTracking
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<DataContext>(opt =>
+                opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+            );
 
             var app = builder.Build();
 

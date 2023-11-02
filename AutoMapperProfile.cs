4618261ef6cab4a7d2ad785ced69b6239580a403
Natan <natan-moreira1@hotmail.com>
2023-11-01 23:10:35 -0300

    chore: config auto mapper

---
+++ AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using StockTracking.DTOs.Stock;
+using StockTracking.Models;
+
+namespace StockTracking
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Stock, StockDTO>();
+        }
+    }
+}
